26094d3c51921b179c89ca5c4341046370bdae4c
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-12-21 15:40:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 A = pd.read_csv("train.csv") 
-B = pd.read_csv("test.csv") 
+#B = pd.read_csv("test.csv") 
 
 # PAGE D'ACCUEIL
 st.title("Analyse et Prediction des Prix de Vente d'une Maison")
@@ -13,7 +15,13 @@ st.markdown("""Ce Dashboard explore les donnees sur les maisons a AMES, IOWA""")
 st.header("Analyse Exploratoire")
 st.subheader("Apercu des donnees")
 st.write(A.head()) 
-st.write(B.head()) 
+
+#VISUALISATION 
+st.subheader("Distribution des prix de vente")
+fig, ax = plt.subplots()
+sn.hisplot(train['SalePrice'], kde=True, ax=ax)
+st.pyplot(fig)
+
 
 
 
