7afae5ff732dc9db28615aad3bf906923d6f5211
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-12-21 15:23:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sn
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
-#from sklearn.metrics import mean_squared_error, r2_score
-#from sklearn.preprocessing import LabelEncoder
-#from sklearn.preprocessing import StandardScaler
+
 
 # PAGE D'ACCUEIL
 st.title("Analyse et Prediction des Prix de Vente d'une Maison")
+st.markdown("""Ce Dashboard explore les donnees sur les maisons a AMES, IOWA""")
+
+
 
