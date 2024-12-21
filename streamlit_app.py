77456fd1494569fc17174d50891683de63e89d11
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-12-21 18:57:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,71 @@ st.subheader("Apercu des donnees")
 st.write(A.head()) 
 
 #VISUALISATION 
-st.subheader("Analyse de correlation")
-#corr = train.corr() 
-#fig, ax = plt.subplots(figsize=(10, 8))
-#sn.heatmap(corr, annot=False, cmap="coolwarm", ax=ax)
-#st.pyplot(fig)
+#st.subheader("Analyse de correlation")
 
-#fig, ax = st.subplots()
-#st.hisplot(A['SalePrice'], kde=True, ax=ax)
-#st.pyplot(fig)
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import altair as alt 
+
+
+
+ 
+# Charger les données
+df = pd.read_csv('Iris.csv', delimiter=';')
+
+
+# Créer un titre
+st.title("Mon premier tableau de bord Streamlit\n")
+
+ 
+# Afficher les données dans un tableau
+# st.table(df)
+
+#creer un char Altair
+chart = alt.Chart(df).mark_bar().encode( x='PetalLength' , y='PetalWidth')
+
+#creer un char Altair
+chart = alt.Chart(df).mark_bar().encode( x='SepalLength' , y='SepalWidth')
+
+#Afficher le chart sur Streamlit
+st.altair_chart(chart, use_container_width=True)
+
+chart = alt.Chart(df).mark_point().encode(x='SepalLength' , y='PetalLength')
+#Afficher le chart sur Streamlit
+st.altair_chart(chart, use_container_width=True) 
+
+#les 05 premieres lignes de notre tableau
+st.write(df.head())
+
+with st.sidebar:
+ st.title('Iris visualisation') 
+ st.sidebar.subheader("differentes fonctionnalites de notre DASHBOARD :")
+
+#definir un bouton avec Action
+#def afficher_messsage():
+ #st.write("Bienvenur sur notre Tableau de Bord!")
+  #if st.sidebar.button("Commencer")
+   #afficher_message()
+ 
+ #texte d'entree
+st.sidebar.text_input("entrer une valeur") 
+
+#pour le curseur
+iris = st.sidebar.slider("Setosa:", 18, 100, 25) 
+st.write(f"Vous avez selectionne: {iris} ")
+
+#pour selectionner les cases
+with st.sidebar.container():
+ st.write("Section 1")
+ st.radio("Choix :", ["SepalLength","SepalWidth","PetalLength","PetalWith","Species"])
+
+#selecteur de liste
+#st.sidebar.selectbox("Setosa:", 18, 100, 25) 
+st.sidebar.multiselect("Choix:", ["SepalLenght","SepalWidth","PetalLengh","PetalWidth","Species"])
+
+
+
 
 
 
