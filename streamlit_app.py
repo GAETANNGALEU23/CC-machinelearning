64777521fddc3906b7bf81e3a8c90c5e89ed5852
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-12-21 15:10:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sn
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
+#import seaborn as sn
+#from sklearn.model_selection import train_test_split
+#from sklearn.linear_model import LinearRegression
+#from sklearn.metrics import mean_squared_error, r2_score
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.preprocessing import StandardScaler
 
 #chargement des donnees
 def load_data():
